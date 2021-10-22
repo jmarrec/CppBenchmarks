e6ccd93c1c8210f8c6390430b9287c32e2eae148
Julien Marrec <julien.marrec@gmail.com>
2021-10-22 16:00:46 +0200

    Found the issue with the reduce: it really must be associative and communitative as the order of the binaryOp arguments can be switched

--- bench_stl_parallel2.cpp
+++ bench_stl_parallel2.cpp
@@ -11,6 +11,15 @@
 #include <thread>
 #include <vector>
 
+// NOTE: you have to be VERY carful with std::reduce. This looks ok, doesn't it?
+// double total = std::reduce(std::execution::seq, vec.begin(), vec.end(), 0.0, [](double sum, double arg) { return sum + std::tan(arg); });
+// Well it's **terribly** wrong. Unlike accumulate, the order of the two args to the binary op are unspecified, so you MUST use something that is
+// fully associative and commutative
+// REDUCE: BinaryOp applies the function in an unspecified order!
+// binary_op - binary FunctionObject that will be applied in unspecified order
+// to the result of dereferencing the input iterators,
+// the results of other binary_op and init.
+
 std::vector<double> makeVector(int64_t lenVector) {
 
   std::vector<double> randValues;
@@ -33,18 +42,37 @@ double calculateTotal(const std::vector<double>& vec) {
   return total;
 }
 
+void checkTotal(const double& expectedTotal, const double& total, benchmark::State& state) {
+  if (std::abs(expectedTotal - total) > 0.0001) {
+    std::string msg = "Wrong calculation: Expected total=" + std::to_string(expectedTotal) + ", got " + std::to_string(total) + ", error is "
+                      + std::to_string(std::abs(expectedTotal - total));
+    state.SkipWithError(msg.c_str());
+  }
+}
+
 static void BM_Manual(benchmark::State& state) {
   auto vec = makeVector(state.range(0));
-  double checkTotal = calculateTotal(vec);
+  double expectedTotal = calculateTotal(vec);
 
   // Code inside this loop is measured repeatedly
   for (auto _ : state) {
     double total = calculateTotal(vec);
-    benchmark::DoNotOptimize(total);
-    if (std::abs(checkTotal - total) > 0.0001) {
-      std::string msg = "Wrong calculation: error is " + std::to_string(std::abs(checkTotal - total));
-      state.SkipWithError(msg.c_str());
-    }
+
+    checkTotal(expectedTotal, total, state);
+  }
+
+  state.SetComplexityN(state.range(0));
+}
+
+static void BM_Accumulate(benchmark::State& state) {
+  auto vec = makeVector(state.range(0));
+  double expectedTotal = calculateTotal(vec);
+
+  // Code inside this loop is measured repeatedly
+  for (auto _ : state) {
+    double total = std::accumulate(vec.begin(), vec.end(), 0.0, [](double sum, double arg) { return sum + std::tan(arg); });
+
+    checkTotal(expectedTotal, total, state);
   }
 
   state.SetComplexityN(state.range(0));
@@ -52,17 +80,15 @@ static void BM_Manual(benchmark::State& state) {
 
 static void BM_Seq(benchmark::State& state) {
   auto vec = makeVector(state.range(0));
-  double checkTotal = calculateTotal(vec);
+  double expectedTotal = calculateTotal(vec);
 
   // Code inside this loop is measured repeatedly
   for (auto _ : state) {
-    double total = std::reduce(std::execution::seq, vec.begin(), vec.end(), 0.0, [](double sum, double arg) { return sum + std::tan(arg); });
-    benchmark::DoNotOptimize(total);
+    auto workVec = vec;  // That must be an expensive copy
+    std::transform(std::execution::seq, workVec.begin(), workVec.end(), workVec.begin(), [](double arg) { return std::tan(arg); });
+    double total = std::reduce(std::execution::seq, workVec.begin(), workVec.end(), 0.0);
 
-    if (std::abs(checkTotal - total) > 0.0001) {
-      std::string msg = "Wrong calculation: error is " + std::to_string(std::abs(checkTotal - total));
-      state.SkipWithError(msg.c_str());
-    }
+    checkTotal(expectedTotal, total, state);
   }
 
   state.SetComplexityN(state.range(0));
@@ -70,17 +96,15 @@ static void BM_Seq(benchmark::State& state) {
 
 static void BM_Par(benchmark::State& state) {
   auto vec = makeVector(state.range(0));
-  double checkTotal = calculateTotal(vec);
+  double expectedTotal = calculateTotal(vec);
 
   // Code inside this loop is measured repeatedly
   for (auto _ : state) {
-    double total = std::reduce(std::execution::unseq, vec.begin(), vec.end(), 0.0, [](double sum, double arg) { return sum + std::tan(arg); });
-    benchmark::DoNotOptimize(total);
+    auto workVec = vec;  // That must be an expensive copy
+    std::transform(std::execution::unseq, workVec.begin(), workVec.end(), workVec.begin(), [](double arg) { return std::tan(arg); });
+    double total = std::reduce(std::execution::seq, workVec.begin(), workVec.end(), 0.0);
 
-    if (std::abs(checkTotal - total) > 0.0001) {
-      std::string msg = "Wrong calculation: error is " + std::to_string(std::abs(checkTotal - total));
-      state.SkipWithError(msg.c_str());
-    }
+    checkTotal(expectedTotal, total, state);
   }
 
   state.SetComplexityN(state.range(0));
@@ -88,23 +112,41 @@ static void BM_Par(benchmark::State& state) {
 
 static void BM_Par_Unseq(benchmark::State& state) {
   auto vec = makeVector(state.range(0));
-  double checkTotal = calculateTotal(vec);
+  double expectedTotal = calculateTotal(vec);
 
   // Code inside this loop is measured repeatedly
   for (auto _ : state) {
-    double total = std::reduce(std::execution::par_unseq, vec.begin(), vec.end(), 0.0, [](double sum, double arg) { return sum + std::tan(arg); });
-    benchmark::DoNotOptimize(total);
+    auto workVec = vec;  // That must be an expensive copy
+    std::transform(std::execution::par_unseq, workVec.begin(), workVec.end(), workVec.begin(), [](double arg) { return std::tan(arg); });
+    double total = std::reduce(std::execution::seq, workVec.begin(), workVec.end(), 0.0);
 
-    if (std::abs(checkTotal - total) > 0.0001) {
-      std::string msg = "Wrong calculation: error is " + std::to_string(std::abs(checkTotal - total));
-      state.SkipWithError(msg.c_str());
-    }
+    checkTotal(expectedTotal, total, state);
+  }
+
+  state.SetComplexityN(state.range(0));
+}
+
+static void BM_Par_Unseq_Transform_Reduce(benchmark::State& state) {
+  auto vec = makeVector(state.range(0));
+  double expectedTotal = calculateTotal(vec);
+
+  // Code inside this loop is measured repeatedly
+  for (auto _ : state) {
+    double total = std::transform_reduce(std::execution::par_unseq, vec.begin(), vec.end(), 0.0,
+                                         // Reduce operation:
+                                         std::plus<>(),
+                                         // transform operation
+                                         [](double arg) { return std::tan(arg); });
+
+    checkTotal(expectedTotal, total, state);
   }
 
   state.SetComplexityN(state.range(0));
 }
 
 BENCHMARK(BM_Manual)->Range(8, 8 << 17)->UseRealTime()->Complexity();
+BENCHMARK(BM_Accumulate)->Range(8, 8 << 17)->UseRealTime()->Complexity();
 BENCHMARK(BM_Seq)->Range(8, 8 << 17)->UseRealTime()->Complexity();
 BENCHMARK(BM_Par)->Range(8, 8 << 17)->UseRealTime()->Complexity();
 BENCHMARK(BM_Par_Unseq)->Range(8, 8 << 17)->UseRealTime()->Complexity();
+BENCHMARK(BM_Par_Unseq_Transform_Reduce)->Range(8, 8 << 17)->UseRealTime()->Complexity();
