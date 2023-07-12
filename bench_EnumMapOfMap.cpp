558eef6f641f8f26ef587e58d13ff10a26f0d2d8
Julien Marrec <julien.marrec@gmail.com>
2023-07-12 10:57:04 +0200

    Use another unordered_map as the inner value

---
+++ bench_EnumMapOfMap.cpp
@@ -0,0 +1,117 @@
+#include <benchmark/benchmark.h>
+
+#include <array>
+#include <map>
+#include <random>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include "IddEnums.hpp"
+
+static constexpr int64_t N_times_each = 5;
+
+using MapValueType = std::unordered_map<std::string, std::string>;
+
+int random_number(int max_size) {
+  std::random_device rd;                                                          //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd());                                                         //Standard mersenne_twister_engine seeded with rd()
+  std::uniform_int_distribution<std::mt19937::result_type> distrib(0, max_size);  // Generates an int in the closed interval [0, n]
+  int index = distrib(rd);
+  return index;
+}
+
+std::string getKey(const IddObjectType& iddObjectType) {
+  return iddObjectType.valueName() + std::to_string(random_number(2));
+}
+
+IddObjectType randomIddObjectType() {
+  return {random_number(static_cast<int>(IddObjectType::CommentOnly))};
+}
+
+static void BM_Current(benchmark::State& state) {
+
+  for (auto _ : state) {
+    using IddObjectTypeMap = std::map<IddObjectType, MapValueType>;
+    IddObjectTypeMap m_iddObjectTypeMap;
+    for (int64_t i = 0; i < N_times_each * state.range(0); ++i) {
+      IddObjectType iddObjectType = randomIddObjectType();
+      m_iddObjectTypeMap[iddObjectType].insert(std::make_pair(getKey(iddObjectType), iddObjectType.valueName()));
+    }
+  }
+
+  state.SetComplexityN(state.range(0));
+}
+
+struct IddObjectTypeHash
+{
+  /* constexpr */ std::size_t operator()(IddObjectType const& iddObjectType) const noexcept {
+    return std::hash<int>{}(iddObjectType.value());
+  }
+};
+
+static void BM_HashMap(benchmark::State& state) {
+
+  for (auto _ : state) {
+    using IddObjectTypeMap = std::unordered_map<IddObjectType, MapValueType, IddObjectTypeHash>;
+    IddObjectTypeMap m_iddObjectTypeMap;
+    for (int64_t i = 0; i < N_times_each * state.range(0); ++i) {
+      IddObjectType iddObjectType = randomIddObjectType();
+      m_iddObjectTypeMap[iddObjectType].insert(std::make_pair(getKey(iddObjectType), iddObjectType.valueName()));
+    }
+  }
+
+  state.SetComplexityN(state.range(0));
+}
+
+static void BM_HashMapReserve(benchmark::State& state) {
+
+  for (auto _ : state) {
+    using IddObjectTypeMap = std::unordered_map<IddObjectType, MapValueType, IddObjectTypeHash>;
+    IddObjectTypeMap m_iddObjectTypeMap;
+    m_iddObjectTypeMap.reserve(static_cast<int>(IddObjectType::CommentOnly) + 1);
+
+    for (int64_t i = 0; i < N_times_each * state.range(0); ++i) {
+      IddObjectType iddObjectType = randomIddObjectType();
+      m_iddObjectTypeMap[iddObjectType].insert(std::make_pair(getKey(iddObjectType), iddObjectType.valueName()));
+    }
+  }
+
+  state.SetComplexityN(state.range(0));
+}
+
+static void BM_Vector(benchmark::State& state) {
+
+  for (auto _ : state) {
+    using IddObjectTypeMap = std::vector<MapValueType>;
+    IddObjectTypeMap m_iddObjectTypeMap;
+    m_iddObjectTypeMap.resize(static_cast<int>(IddObjectType::CommentOnly) + 1);
+
+    for (int64_t i = 0; i < N_times_each * state.range(0); ++i) {
+      IddObjectType iddObjectType = randomIddObjectType();
+      m_iddObjectTypeMap[iddObjectType.value()].insert(std::make_pair(getKey(iddObjectType), iddObjectType.valueName()));
+    }
+  }
+
+  state.SetComplexityN(state.range(0));
+}
+
+static void BM_Array(benchmark::State& state) {
+
+  for (auto _ : state) {
+    using IddObjectTypeMap = std::array<MapValueType, static_cast<size_t>(IddObjectType::CommentOnly) + 1>;
+    IddObjectTypeMap m_iddObjectTypeMap;
+
+    for (int64_t i = 0; i < N_times_each * state.range(0); ++i) {
+      IddObjectType iddObjectType = randomIddObjectType();
+      m_iddObjectTypeMap[iddObjectType.value()].insert(std::make_pair(getKey(iddObjectType), iddObjectType.valueName()));
+    }
+  }
+
+  state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK(BM_Current)->RangeMultiplier(4)->Range(8, 8 << 16)->Complexity()->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_HashMap)->RangeMultiplier(4)->Range(8, 8 << 16)->Complexity()->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_HashMapReserve)->RangeMultiplier(4)->Range(8, 8 << 16)->Complexity()->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_Vector)->RangeMultiplier(4)->Range(8, 8 << 16)->Complexity()->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_Array)->RangeMultiplier(4)->Range(8, 8 << 16)->Complexity()->Unit(benchmark::kMillisecond);
